10c5816d86f1df2232dbb32ce6caff83e1d889b2
adrianacostep33 <adriana.coste@principal33.com>
2024-04-03 14:30:41 +0300

    Display dummy data in expenses overview screens

--- expense-tracker/components/ExpensesOutput/ExpensesList.js
+++ expense-tracker/components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, Text } from "react-native";
 import React from "react";
 
-const ExpensesList = () => {
-  return <FileList />;
+const ExpensesList = ({ expenses }) => {
+  function renderExpenseItem(itemData) {
+    return <Text>{itemData.item.description}</Text>;
+  }
+
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 };
 
 export default ExpensesList;
--- expense-tracker/components/ExpensesOutput/ExpensesSummary.js
+++ expense-tracker/components/ExpensesOutput/ExpensesSummary.js
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const ExpensesSummary = () => {
+const ExpensesSummary = ({ expenses, periodName }) => {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
   return (
     <View>
-      <Text>Last 7 Days</Text>
-      <Text>$1</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 };
