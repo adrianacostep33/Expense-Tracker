45a17110b077e64a7dd6c0c4e62522e5066c0587
adrianacostep33 <adriana.coste@principal33.com>
2024-04-03 14:08:48 +0300

    Add dummy expenses output components

---
+++ expense-tracker/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ExpensesList = () => {
+  return <FileList />;
+};
+
+export default ExpensesList;
+
+const styles = StyleSheet.create({});
