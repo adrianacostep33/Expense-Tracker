f97cf8419e083a9eaf878cf346840aaa68fbdf8e
adrianacostep33 <adriana.coste@principal33.com>
2024-04-03 20:30:20 +0300

    Add styled expense form

--- expense-tracker/components/ManageExpense/ExpenseForm.js
+++ expense-tracker/components/ManageExpense/ExpenseForm.js
@@ -1,15 +1,78 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import Input from "./Input";
+import { GlobalStyles } from "../../constants/styles";
 
 const ExpenseForm = () => {
+  function amountChangeHandler() {}
+
   return (
-    <View>
-      <Input />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLangth: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          //   autoCorrect: true
+          //   autoCapitalize:'words'
+          onChangeText: () => {},
+        }}
+      />
     </View>
   );
 };
 
 export default ExpenseForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  errorText: {
+    textAlign: "center",
+    color: GlobalStyles.colors.error500,
+    margin: 8,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
--- expense-tracker/screens/ManageExpense.js
+++ expense-tracker/screens/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -45,6 +46,7 @@ const ManageExpense = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttonContainer}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
